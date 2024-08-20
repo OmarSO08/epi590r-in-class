707b0223315df3425cf531d1404125e619fd3e1d
OmarSO08 <omar.souabni@emory.edu>
2024-08-20 15:40:51 -0400

    function making

---
+++ function creation.R
@@ -0,0 +1,22 @@
+
+square <- function(x) {
+	sqr <- x^2
+	return(sqr)
+}
+# test it out
+square(7)
+
+
+x <- c (2 , 45, 665, 84)
+square(nlsy$sleep_wknd)
+
+raise <- function(x, power=2) {
+		pr <- x ^ power
+		 rst <- cat ("the result is equalt to", pr,"\n")
+		return(rst)
+
+		}
+
+
+# test with
+raise(2,3)
